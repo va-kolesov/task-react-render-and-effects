e23ae4d6d93116759698515d161b0489c47b6632
Vadim <60597212+va-kolesov@users.noreply.github.com>
2022-12-26 23:40:40 +0300

    va-kolesov/task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,47 @@
+import { Component } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
-export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+interface IProps {
+    sourceId: string;
+}
+interface IState {
+    message: number;
+}
+
+export class Effects extends Component<IProps, IState> {
+    props: IProps;
+    state: IState;
+    _callback: (ms: number) => void;
+    constructor(props: { sourceId: string }) {
+        super(props);
+        this.props = props;
+        this.state = { message: -1 };
+        this._callback = this.callback.bind(this);
+    }
+    componentDidMount(): void {
+        subscribe(this.props.sourceId, this._callback);
+    }
+    componentDidUpdate(
+        prevProps: Readonly<IProps>,
+        prevState: Readonly<IState>,
+    ): void {
+        if (prevProps.sourceId !== this.props.sourceId) {
+            unsubscribe(prevProps.sourceId, this._callback);
+            subscribe(this.props.sourceId, this._callback);
+            this.setState({ message: -1 });
+        }
+    }
+    componentWillUnmount(): void {
+        unsubscribe(this.props.sourceId, this._callback);
+    }
+    callback(ms: number) {
+        this.setState({ message: ms });
+    }
+    render() {
+        return (
+            <div>
+                {this.props.sourceId}: {this.state.message}
+            </div>
+        );
+    }
 }
